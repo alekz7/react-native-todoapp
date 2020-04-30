cf74d7a80cf15598b4ac9c8dc0b8453be346636d
Alejandro Aragon <ing.aragon.ipn@gmail.com>
2020-04-30 11:38:51 -0500

    todo app react native basic controls

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem'
 export default function App() {
+
+  const [todos,setTodos] = useState([
+    { text: 'comprar cafe', key: '1'},
+    { text: 'ir al super', key: '2'},
+    { text: 'ir al pan', key: '3'}
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item })=>(
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>    
     </View>
   );
 }
@@ -12,8 +36,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'    
   },
+  content:{
+    padding: 40,
+  },
+  list:{
+    marginTop:20
+  }
+
 });
